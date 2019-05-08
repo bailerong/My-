eaf7d96f8f5ef34767d3cadac1f6b0ca6782bfec
bailerong <15929276532@163.com>
2019-05-08 22:49:42 +0800

    这是一个关于我们的多态的方法调用

---
+++ Test.java
@@ -0,0 +1,27 @@
+class Base{
+    public Base(){
+        g();
+    }
+    public void f(){
+        System.out.println("Base f()");
+    }
+    public void g(){
+        System.out.println("Base g()");
+    }
+
+}
+class Derived extends Base{
+    public void f(){
+        System.out.println("Derived f()");
+    }
+    public void g(){
+        System.out.println("Derived g()");
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        Base b=new Derived();
+        b.f();
+        b.g();
+    }
+}
